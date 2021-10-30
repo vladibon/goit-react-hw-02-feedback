214e7a0c2a423d6ca31d6bc97814703c0cfd8af6
vladibon <vladi.bondar@gmail.com>
2021-10-30 23:10:18 +0000

    Deploying to gh-pages from @ vladibon/react-app@a44fb51fd408cf871902b613f3936d427ab0a291 🚀

--- static/js/main.f37beb08.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-app"]=this["webpackJsonpreact-app"]||[]).push([[0],{10:function(t,e,c){"use strict";c.r(e);var n=c(1),r=c.n(n),a=c(3),s=c.n(a),p=(c(8),c(0));var i=function(){return Object(p.jsx)("div",{className:"App"})};s.a.render(Object(p.jsx)(r.a.StrictMode,{children:Object(p.jsx)(i,{})}),document.getElementById("root"))},8:function(t,e,c){}},[[10,1,2]]]);
-//# sourceMappingURL=main.f37beb08.chunk.js.map
