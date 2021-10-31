55497bf428cec0b78e7b44e21fe620eaf5798049
Vladimir Bondar <vladi.bondar@gmail.com>
2021-11-01 01:12:38 +0200

    add App class

--- src/App.js
+++ src/App.js
@@ -1,5 +1,69 @@
-function App() {
-  return <div className='App'></div>;
+import { Component } from 'react';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleGoodFeedback = () => {
+    this.setState(prevState => ({
+      good: prevState.good + 1,
+    }));
+  };
+
+  handleNeutralFeedback = () => {
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+    }));
+  };
+
+  handleBadFeedback = () => {
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }));
+  };
+
+  render() {
+    return (
+      <div className='feedbacks'>
+        <h2>Please leave feedback</h2>
+
+        <div className='buttons'>
+          <button
+            className='good'
+            type='button'
+            onClick={this.handleGoodFeedback}
+          >
+            Good
+          </button>
+          <button
+            className='neutral'
+            type='button'
+            onClick={this.handleNeutralFeedback}
+          >
+            Neutral
+          </button>
+          <button
+            className='bad'
+            type='button'
+            onClick={this.handleBadFeedback}
+          >
+            Bad
+          </button>
+        </div>
+
+        <h2>Statistics</h2>
+
+        <div>
+          <span>Good: {this.state.good}</span>
+          <span>Neutral: {this.state.neutral}</span>
+          <span>Bad: {this.state.bad}</span>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import 'modern-normalize/modern-normalize.css';
 import './index.css';
-import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
