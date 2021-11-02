1eb06c406c861ea2c7a7530a3ad59f22156cba59
Vladimir Bondar <vladi.bondar@gmail.com>
2021-11-02 18:26:02 +0200

    fix Statistics markup

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,26 +1,31 @@
 import PropTypes from 'prop-types';
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
-  <table>
-    <thead>
-      <tr>
-        <th>Good</th>
-        <th>Neutral</th>
-        <th>Bad</th>
-        <th>Total</th>
-        <th>Positive feedback</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>{good}</td>
-        <td>{neutral}</td>
-        <td>{bad}</td>
-        <td>{total}</td>
-        <td>{positivePercentage}%</td>
-      </tr>
-    </tbody>
-  </table>
+  <>
+    <table>
+      <thead>
+        <tr>
+          <th>Good</th>
+          <th>Neutral</th>
+          <th>Bad</th>
+          <th>Total</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{good}</td>
+          <td>{neutral}</td>
+          <td>{bad}</td>
+          <td>{total}</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <h3>Positive feedback:</h3>
+    <div>
+      <span>{positivePercentage}%</span>
+    </div>
+  </>
 );
 
 Statistics.propTypes = {
