c1eb87f03ba7c6fef71bfc46f15015526f673505
Vladimir Bondar <vladi.bondar@gmail.com>
2021-11-02 14:31:33 +0200

    fix

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 const Section = ({ title, children }) => (
   <section>
-    <h2>{title}</h2>
+    {title && <h2>{title}</h2>}
     {children}
   </section>
 );
